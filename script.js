c569915eb7cd5d5bd46dba9904a7a5483165f702
FahrizaIlhamiDevLens <coganreza933@gmail.com>
2026-03-27 18:11:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// ambil form login
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    // supaya halaman tidak reload
+    event.preventDefault();
+
+    // ambil input user
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // ======================
+    // ISI USERNAME & PASSWORD DISINI
+    // ======================
+
+    const correctUsername = "getid";
+    const correctPassword = "202100";
+
+    // cek login
+    if (username === correctUsername && password === correctPassword) {
+      // buka tab baru menuju website
+      window.open("https://only-for-you-bice.vercel.app/", "_blank");
+    } else {
+      alert("anda salah");
+    }
+  });
